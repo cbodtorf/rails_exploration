906d666699fd85d07bc8be4fa1acd9228598f8df
Caleb Bodtorf <caleb@e4consults.com>
2017-02-21 16:33:17 -0500

    In order to meet the Epic Mode requirements, I placed all of the questions into a data structure (ie. an Array). I am now iterating over said array with the each_with_index method so That I don't have to manually enter each number.

--- basics.rb
+++ basics.rb
@@ -1,43 +1,36 @@
 ## Difficult Mode:
 ### Question method
-def question(prompt,body)
+def questionMethod(prompt,body)
   print prompt
   puts body
 end
 
-question("1. ", 4 == 2 * 2)
-question("2. ", 5 > 4)
-question("3. ", (3 * 4) > 10)
-question("4. ", 3 == 3)
-question("5. ", (1 > 0) && (1 <= 1) == true)
-question("6. ", 21 != 2)
-question("7. ", (14 <= 10) || (3 >= 3) == true)
-question("8. ", 10 % 2 == 0)
-question("9. ", 100 == 10 ** 2)
-question("10. ", 6 == 1000 / (300 / 2))
-
-#I AM A COMMENT RUBY IGNORES ME BECAUSE MY LINE STARTED WITH A '#'
-#Use the following variable msg for questions 11 - 15
 msg = "hEllO"
-
-question("11. ", msg.upcase == "HELLO")
-question("12. ", msg.swapcase == "HeLLo")
-question("13. ", msg.reverse == "OllEh")
-question("13a. ", msg.upcase.reverse == "OLLEH")
-
 arr = ['a', 'b', 'c']
-
-question("14. ", arr[2] == 'c')
-question("15. ", 3 == arr.count)
-question("16. ", arr.join(',') == 'a,b,c')
-
-#Hint: This will take 2 method calls
-
-question("17. ", arr.last.upcase == 'C')
-
 hsh = { name: 'Fido', age: '99' }
 
-question("18. ", hsh[:name] == 'Fido')
-question("19. ", hsh.keys == [:name, :age])
-question("20. ", hsh.values == ['Fido', '99'])
-question("21. ", hsh.merge({:special => "Dog"}) == { name: 'Fido', age: '99', special: 'Dog' })
+dataQuestions = [ 4 == 2 * 2,
+                  5 > 4,
+                  (3 * 4) > 10,
+                  3 == 3,
+                  (1 > 0) && (1 <= 1) == true,
+                  21 != 2,
+                  (14 <= 10) || (3 >= 3) == true,
+                  10 % 2 == 0,
+                  100 == 10 ** 2,
+                  6 == 1000 / (300 / 2),
+                  msg.upcase == "HELLO",
+                  msg.swapcase == "HeLLo",
+                  msg.reverse == "OllEh",
+                  msg.upcase.reverse == "OLLEH",
+                  arr[2] == 'c',
+                  3 == arr.count,
+                  arr.join(',') == 'a,b,c',
+                  arr.last.upcase == 'C',
+                  hsh[:name] == 'Fido',
+                  hsh.keys == [:name, :age],
+                  hsh.values == ['Fido', '99'],
+                  hsh.merge({:special => "Dog"}) == { name: 'Fido', age: '99', special: 'Dog' }
+                ]
+
+dataQuestions.each_with_index { |q, idx| questionMethod("#{idx + 1}. ", q) }
