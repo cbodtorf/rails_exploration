d3ba787387a6010cea00b955377b06c89e2ae250
Caleb Bodtorf <caleb@e4consults.com>
2017-02-21 16:09:13 -0500

    added a question method to reduce some of the redundant print and puts method call. This was to meet the difficult mode requirements.

--- basics.rb
+++ basics.rb
@@ -1,74 +1,43 @@
-print "1. "
-puts 4 == 2 * 2
+## Difficult Mode:
+### Question method
+def question(prompt,body)
+  print prompt
+  puts body
+end
 
-print "2. "
-puts 5 > 4
-
-print "3. "
-puts (3 * 4) > 10
-
-print "4. "
-puts 3 == 3
-
-print "5. "
-puts (1 > 0) && (1 <= 1) == true
-
-print "6. "
-puts 21 != 2
-
-print "7. "
-puts (14 <= 10) || (3 >= 3) == true
-
-print "8. "
-puts 10 % 2 == 0
-
-print "9. "
-puts 100 == 10 ** 2
-
-print "10. "
-puts 6 == 1000 / (300 / 2)
+question("1. ", 4 == 2 * 2)
+question("2. ", 5 > 4)
+question("3. ", (3 * 4) > 10)
+question("4. ", 3 == 3)
+question("5. ", (1 > 0) && (1 <= 1) == true)
+question("6. ", 21 != 2)
+question("7. ", (14 <= 10) || (3 >= 3) == true)
+question("8. ", 10 % 2 == 0)
+question("9. ", 100 == 10 ** 2)
+question("10. ", 6 == 1000 / (300 / 2))
 
 #I AM A COMMENT RUBY IGNORES ME BECAUSE MY LINE STARTED WITH A '#'
 #Use the following variable msg for questions 11 - 15
 msg = "hEllO"
 
-print "11. "
-puts msg.upcase == "HELLO"
-
-print "12. "
-puts msg.swapcase == "HeLLo"
-
-print "13. "
-puts msg.reverse == "OllEh"
-
-print "13a. "
-puts msg.upcase.reverse == "OLLEH"
+question("11. ", msg.upcase == "HELLO")
+question("12. ", msg.swapcase == "HeLLo")
+question("13. ", msg.reverse == "OllEh")
+question("13a. ", msg.upcase.reverse == "OLLEH")
 
 arr = ['a', 'b', 'c']
 
-print "14. "
-puts arr[2] == 'c'
-
-print "15. "
-puts 3 == arr.count
-
-print "16. "
-puts arr.join(',') == 'a,b,c'
+question("14. ", arr[2] == 'c')
+question("15. ", 3 == arr.count)
+question("16. ", arr.join(',') == 'a,b,c')
 
 #Hint: This will take 2 method calls
-print "17. "
-puts arr.last.upcase == 'C'
+
+question("17. ", arr.last.upcase == 'C')
 
 hsh = { name: 'Fido', age: '99' }
-print "18. "
-puts  hsh[:name] == 'Fido'
 
-print "19. "
-puts hsh.keys == [:name, :age]
-
-print "20. "
-puts hsh.values == ['Fido', '99']
-
-
-print "21. "
-puts hsh.merge({:special => "Dog"}) == { name: 'Fido', age: '99', special: 'Dog' }
+question("18. ", hsh[:name] == 'Fido')
+question("19. ", hsh.keys == [:name, :age])
+question("20. ", hsh.values == ['Fido', '99'])
+question("21. ", hsh.merge({:special => "Dog"}) == { name: 'Fido', age: '99', special: 'Dog' })
