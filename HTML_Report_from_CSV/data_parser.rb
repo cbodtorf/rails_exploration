fca8770a2ce99810c89dae024f38260e5d22dc27
Caleb Bodtorf <caleb@e4consults.com>
2017-02-22 10:18:38 -0500

    started work on the report.erb. I have it successfully outputting an index-outpout.html file, and the beginnings of a table that will display our data.

--- HTML_Report_from_CSV/data_parser.rb
+++ HTML_Report_from_CSV/data_parser.rb
@@ -1,18 +1,20 @@
 # data parsing
 require 'csv'
+require 'erb'
 
 # CSV.foreach("./planet_express_logs.csv", headers: true) do |row|
 #   puts  row["Money"]
 # end
 
 class WeeklyReport
+  TITLE = "Weekly Report"
 
   def initialize(file_location)
     @jobs = CSV.read(file_location, headers: true, header_converters: :symbol)
   end
 
   def getRevenue
-    puts @jobs[:money].map(&:to_i).reduce(:+)
+    return @jobs[:money].map(&:to_i).reduce(:+)
   end
 
   def getPilotBonus(pilot)
@@ -20,13 +22,17 @@ class WeeklyReport
               .map{|job| job[:money].to_i}
               .reduce(:+) * 0.1
 
-    puts "#{pilot}'s bonus is: #{bonus}"
+    return "#{pilot}'s bonus is: #{bonus}"
   end
 
   def getPilotTripCount(pilot)
     count = @jobs.select{|job| job[:pilot] == pilot }.count
 
-    puts "#{pilot}'s trip count is: #{count}"
+    return "#{pilot}'s trip count is: #{count}"
+  end
+
+  def getHeaders
+    return @jobs.headers
   end
 
 end
@@ -44,3 +50,10 @@ report.getPilotTripCount("Fry")
 report.getPilotTripCount("Leela")
 report.getPilotTripCount("Bender")
 report.getPilotTripCount("Amy")
+
+html_string = File.read("./report.erb")
+compiled_html = ERB.new(html_string).result(binding)
+File.open("./index-output.html", "wb") {|file|
+    file.write(compiled_html)
+    file.close()
+}
