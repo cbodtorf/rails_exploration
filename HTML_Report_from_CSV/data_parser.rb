2575940681d443c64bbb5fb2327dbe4b5ce56034
Caleb Bodtorf <caleb@e4consults.com>
2017-02-22 10:26:13 -0500

    cleaned a couple things up.

--- HTML_Report_from_CSV/data_parser.rb
+++ HTML_Report_from_CSV/data_parser.rb
@@ -2,15 +2,15 @@
 require 'csv'
 require 'erb'
 
-# CSV.foreach("./planet_express_logs.csv", headers: true) do |row|
-#   puts  row["Money"]
-# end
-
 class WeeklyReport
   TITLE = "Weekly Report"
 
   def initialize(file_location)
     @jobs = CSV.read(file_location, headers: true, header_converters: :symbol)
+  end
+
+  def getHeaders
+    return @jobs.headers
   end
 
   def getRevenue
@@ -31,25 +31,9 @@ class WeeklyReport
     return "#{pilot}'s trip count is: #{count}"
   end
 
-  def getHeaders
-    return @jobs.headers
-  end
-
 end
 
 report = WeeklyReport.new "./planet_express_logs.csv"
-
-report.getRevenue
-
-report.getPilotBonus("Fry")
-report.getPilotBonus("Leela")
-report.getPilotBonus("Bender")
-report.getPilotBonus("Amy")
-
-report.getPilotTripCount("Fry")
-report.getPilotTripCount("Leela")
-report.getPilotTripCount("Bender")
-report.getPilotTripCount("Amy")
 
 html_string = File.read("./report.erb")
 compiled_html = ERB.new(html_string).result(binding)
