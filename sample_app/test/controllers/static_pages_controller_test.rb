2370ab0a1fff413cb92c88634a1fbbfd18c91a00
Caleb Bodtorf <caleb@e4consults.com>
2017-03-01 15:17:48 -0500

    learned about the setup method in testing, and about assert_select when testing certain html elements. Made got rid of the base layout so that we could experiment with some of the static templates.

--- sample_app/test/controllers/static_pages_controller_test.rb
+++ sample_app/test/controllers/static_pages_controller_test.rb
@@ -1,19 +1,30 @@
 require 'test_helper'
 
+# assert_select method, which lets us test for the presence of a particular HTML tag
+# (sometimes called a “selector”, hence the name)
+
+# setup is run before each test.
+def setup
+  @base_title = "Ruby on Rails Tutorial Sample App"
+end
+
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get static_pages_home_url
     assert_response :success
+    assert_select "title", "Home | #{@base_title}"
   end
 
   test "should get help" do
     get static_pages_help_url
     assert_response :success
+    assert_select "title", "Help | #{@base_title}"
   end
 
   test "should get about" do
     get static_pages_about_url
     assert_response :success
+    assert_select "title", "About | #{@base_title}"
   end
 
 end
