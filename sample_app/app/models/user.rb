5de77313a277fc4544ab6f45f889b8f2dca10e6e
Caleb Bodtorf <caleb@e4consults.com>
2017-03-07 15:40:46 -0500

    added password validation and tests.

--- sample_app/app/models/user.rb
+++ sample_app/app/models/user.rb
@@ -1,13 +1,15 @@
 class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 
-  # before_save { self.email = email.downcase } # or
-  before_save { email.downcase! }
+  before_save { self.email = email.downcase } # or
+  # before_save { email.downcase! }
 
-  validates(:name, presence: true, length: {maximum: 50, minimun: 3})
-  validates(:email, presence: true, length: {maximum: 255, minimun: 3},
-                    uniqueness: { case_sensitive: false },
-                    format: { with: VALID_EMAIL_REGEX })
+  validates(:name, presence: true, length: { maximum: 50 })
+  validates(:email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false })
 
   has_secure_password
+  validates(:password, presence: true, length: { minimum: 6 })
+
 end
--- sample_app/test/models/user_test.rb
+++ sample_app/test/models/user_test.rb
@@ -3,8 +3,7 @@ require 'test_helper'
 class UserTest < ActiveSupport::TestCase
 
   def setup
-    @user = User.new(name: "Example User", email: "user@example.com",
-                     password: "foobar", password_confirmation: "foobar")
+    @user = User.new(name: "Example User", email: "user@example.com", password: "foobar", password_confirmation: "foobar")
   end
 
   test "should be valid" do
@@ -62,4 +61,6 @@ class UserTest < ActiveSupport::TestCase
     @user.save
     assert_equal mixed_case_email.downcase, @user.reload.email
   end
+
+
 end
