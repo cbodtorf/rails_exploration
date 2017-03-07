b9ce10936c0dfa6c3186a00584f9798bc0523466
Caleb Bodtorf <caleb@e4consults.com>
2017-03-07 13:48:42 -0500

    adding validations to the the User model, as well as tests.

--- sample_app/app/models/user.rb
+++ sample_app/app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+  validates(:name, presence: true, length: {maximum: 50, minimun: 3})
+  validates(:email, presence: true, length: {maximum: 255, minimun: 3}, uniqueness: true)
 end
