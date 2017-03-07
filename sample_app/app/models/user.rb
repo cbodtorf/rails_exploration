7bc9d3be929688500c5b66b273dcf9780565d635
Caleb Bodtorf <caleb@e4consults.com>
2017-03-07 14:47:10 -0500

    added further validation in regards to case sensitivity. Also generated a new migration to add index to the users email.

--- sample_app/app/models/user.rb
+++ sample_app/app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 
+  # before_save { self.email = email.downcase } # or
+  before_save { email.downcase! }
+
   validates(:name, presence: true, length: {maximum: 50, minimun: 3})
   validates(:email, presence: true, length: {maximum: 255, minimun: 3},
                     uniqueness: { case_sensitive: false },
