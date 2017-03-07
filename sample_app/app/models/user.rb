9ac3221bd0fd28faa65107655b91d8428120bd2c
Caleb Bodtorf <caleb@e4consults.com>
2017-03-07 14:31:35 -0500

    add some more validations using regex, testing for uniqueness and valid email format.

--- sample_app/app/models/user.rb
+++ sample_app/app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+
   validates(:name, presence: true, length: {maximum: 50, minimun: 3})
-  validates(:email, presence: true, length: {maximum: 255, minimun: 3}, uniqueness: true)
+  validates(:email, presence: true, length: {maximum: 255, minimun: 3},
+                    uniqueness: { case_sensitive: false },
+                    format: { with: VALID_EMAIL_REGEX })
 end
