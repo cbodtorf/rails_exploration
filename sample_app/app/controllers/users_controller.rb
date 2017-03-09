ed77beba02c431eece3e112ec81abcce4acb53d0
Caleb Bodtorf <caleb@e4consults.com>
2017-03-09 15:44:27 -0500

    Started adding some elements to give user feedback when successfully signing up as a new user. Flash message and page redirect.

--- sample_app/app/controllers/users_controller.rb
+++ sample_app/app/controllers/users_controller.rb
@@ -7,7 +7,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      # Handle a successful save.
+      redirect_to @user # shorthand for redirect_to user_url(@user)
+      flash[:success] = "Welcome to the Sample App"
     else
       render 'new'
     end
