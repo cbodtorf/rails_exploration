d33c843ef0f91e1fa17265e23c28b633cf954d99
Caleb Bodtorf <caleb@e4consults.com>
2017-03-10 10:53:23 -0500

    added method to check if a user is logged in, returns the user object

--- sample_app/app/controllers/application_controller.rb
+++ sample_app/app/controllers/application_controller.rb
@@ -1,3 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  # added here so that all controllers inherit (Rails automatically includes in SessionsController)
+  include SessionsHelper
 end
